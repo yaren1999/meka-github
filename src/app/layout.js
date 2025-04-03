6f7177941951465aad5c41f7bb41400437613fdf
yaren1999 <demirciyarenmisra@gmail.com>
2025-04-03 19:56:32 +0300

    Slider responsive düzenlemeleri tamamlandı

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,7 +21,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Header/>
+        <div style={{paddingTop: "50"}}>
         {children}
+        </div>
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Slider from "@/components/Slider/Slider";
+
 
 export default function Home() {
   return (
    <div>
-    
+    <Slider/>
    </div>
   );
 }
